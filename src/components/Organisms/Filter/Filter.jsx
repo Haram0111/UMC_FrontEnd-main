55130f062e90b130d5ea9f8aa2bdc3e0324f6e7f
Haram0111 <5gkfka5@naver.com>
2022-07-19 23:24:48 +0900

    A: Multeful Filter

--- src/components/Organisms/Filter/Filter.jsx
+++ src/components/Organisms/Filter/Filter.jsx
@@ -15,45 +15,46 @@ function Filter(){
     axios
         .get("https://8a951eff-db70-48f9-ab6a-54e97db1cbe9.mock.pstmn.io")
         .then((result) => {
-        setCardInfo(result.data.result);
+          setCardInfo(result.data);
         })
         .catch((err) => {console.log(err)});
     }, []);
 
-    console.log(cardInfo);
+    
 
     return(
         <>
-        <Form.Select onChange={(e) => setIntype(e.target.value)}>
+        <Form.Select onChange={(e) => setFree(e.target.value)}>
           <option value="All">전체</option>
           <option value="charge">유료</option>
           <option value="free">무료</option>
         </Form.Select>        
-        <Form.Select onChange={(e) => setIntype(e.target.value)}>
-          <option value="All">최신순</option>
-          <option value="charge">인기순</option>
+        <Form.Select onChange={(e) => setView(e.target.value)}>
+          <option value="Latest">최신순</option>
+          <option value="Popularity">인기순</option>
         </Form.Select>        
-        <Form.Select onChange={(e) => setIntype(e.target.value)}> 
+        <Form.Select onChange={(e) => setTheme(e.target.value)}> 
           <option value="All">테마별</option>
-          <option value="charge">테마</option>
-          <option value="free">테마</option>
+          <option value="Simple">심플</option>
+          <option value="Neat">깔끔</option>
         </Form.Select>        
-        <Form.Select onChange={(e) => setIntype(e.target.value)}> 
+        <Form.Select onChange={(e) => setLocation(e.target.value)}> 
           <option value="All">장소별</option>
-          <option value="charge">자동차</option>
-          <option value="free">어딘가</option>
-        </Form.Select>        
+          <option value="Car">자동차</option>
+          <option value="Cafe">카페</option>
+        </Form.Select>
+
         <div className="grid">
           {
             cardInfo&&cardInfo.map((i,a)=>{
-              if(Intype === "All"){
+              if(Free === "All"){
                 return(
                   <Card style={{ width: '18rem' }} className="box">
-                    <Card.Img variant="top" src={i.image_url} />
+                    <Card.Img variant="top" src={i.image} />
                     <Card.Body>
-                    <Card.Title>{i.description}</Card.Title>
+                    <Card.Title>{i.title}</Card.Title>
                     <Card.Text>
-                        {i.description}
+                        {i.text}
                     </Card.Text>
                     <Button variant="primary">Go somewhere</Button>
                     </Card.Body>
@@ -61,7 +62,7 @@ function Filter(){
                 );
               }
               else{
-                if(Intype === i.type){
+                if(Free === i.type){
                   return(
                     <Card style={{ width: '18rem' }} className="box">
                       <Card.Img variant="top" src={i.image} />
